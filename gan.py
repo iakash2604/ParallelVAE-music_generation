5192104649903eeceeb07ab1ccfa0ff4e6a2b8c9
Akash Panther <iakash2702@gmail.com>
2018-02-27 14:37:35 +0530

    just a test commit

--- gan.py
+++ gan.py
@@ -1,3 +1,12 @@
 import numpy as np
 import keras
 
+class gan:
+    
+    def __init__(self):
+        return None
+    
+    #TODO
+    #create a generator-discriminator pair. 
+    #protocols for training them 
+    #ability to merge to create bigger models
